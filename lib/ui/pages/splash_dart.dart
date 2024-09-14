dcdaee6275b7cf106a496a27a6fcbf1535653c30
srirahayuapriyani <srirahayuapriyani050403@gmail.com>
2024-09-14 23:05:31 +0700

    baru

---
+++ lib/ui/pages/splash_dart.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({ Key? key, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_plane.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
